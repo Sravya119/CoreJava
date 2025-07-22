3b37ca018273413783d2339900d4711883ad0077
Sravya119 <msravya119@gmail.com>
2025-07-22 16:52:24 +0530

    Form

---
+++ Form.java
@@ -0,0 +1,80 @@
+ public class Form {
+    public static void main(String[] args){
+        short firstYear = 1;
+        boolean transferee = false;
+        boolean returnee = true;
+        String lastName = "Marella";
+        String firstName = "Sravya";
+        String completeAddress = "Bapatla, Andhra Pradesh";
+        String dateOfBirth =  "11/9/2003";
+        String birthPlace = "Guntur";
+        String citizenship = "Others";
+        String sex = "Female";
+        String civilStatus = "Single";
+        long contactNumber = 1122554477;
+        String religion = "Hindu";
+        String spouseName = "No";
+        long spouseContact = 0;
+        String fatherName = "Ankaiah";
+        String fatherOccupation = "Lecturer";
+        long fatherNumber = 1234567890;
+        String motherName = "Siva Parvathi";
+        String motherOccupation = "House Wife";
+        long motherNumber = 1234567890;
+        String guardianName = "N/A";
+        String guardianAddress = "N/A";
+        String guardianNumber = "N/A";
+        String seniorHighSchool = "Little Hearts School";
+        int yearGraduate = 2019;
+        float genAverage = 9.8f;
+        String course = "B.Tech";
+        boolean isAlsPasser_yes = true;
+        boolean isAlsPasser_no = false;
+        int yearTaken = 2022;
+        boolean is4psbeneficiary_yes = true;
+        boolean is4psbeneficiary_no = false;
+        boolean isDisability_yes = false;
+        boolean isDisability_no = true;
+        String disabilityType ="N/A";
+
+
+
+        System.out.println("First Year: " + firstYear);
+        System.out.println("Transferee: " + transferee);
+        System.out.println("Returnee: " + returnee);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("First Name: " + firstName);
+        System.out.println("Address: " + completeAddress);
+        System.out.println("Date: " + dateOfBirth);
+        System.out.println("Birthplace: " + birthPlace);
+        System.out.println("Citizenship: " + citizenship);
+        System.out.println("Civil Status: " + civilStatus);
+        System.out.println("Sex: " + sex);
+        System.out.println("Contact Number: " + contactNumber);
+        System.out.println("Religion: " + religion);
+        System.out.println("Spouse Name: " + spouseName);
+        System.out.println("Spouse Contact: " + spouseContact);
+        System.out.println("Father Name: " + fatherName);
+        System.out.println("Father Occupation: " + fatherOccupation);
+        System.out.println("Father Number: " + fatherNumber);
+        System.out.println("Mother Name: " + motherName);
+        System.out.println("Mother Work: " + motherOccupation);
+        System.out.println("Mother Number: " + motherNumber);
+        System.out.println("Guardian Name: " + guardianName);
+        System.out.println("Guardian Address: " + guardianAddress);
+        System.out.println("Guardian Number: " + guardianNumber);
+        System.out.println("School Name: " + seniorHighSchool);
+        System.out.println("Graduated Year: " + yearGraduate);
+        System.out.println("Average: " + genAverage);
+        System.out.println("Course: " + course);
+        System.out.println("ALS Passer Yes" + isAlsPasser_yes); 
+        System.out.println("ALS Passer No" + isAlsPasser_no); 
+        System.out.println("Year Taken" + yearTaken);
+        System.out.println("4Ps Beneficiary Yes" + is4psbeneficiary_yes); 
+        System.out.println("4Ps Beneficiary No" + is4psbeneficiary_no); 
+        System.out.println("Disability Yes" + isDisability_yes); 
+        System.out.println("Disability No" + isDisability_no); 
+        System.out.println("Disability Type" + disabilityType); 
+
+    }
+}
