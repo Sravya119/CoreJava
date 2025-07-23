4df89424c20d1a50de0b6f39f9747df72f057506
Sravya119 <msravya119@gmail.com>
2025-07-23 17:05:56 +0530

    Task 23-07-25(Monthly EMI, BankAccount)

---
+++ Operators/BankAccount.java
@@ -0,0 +1,36 @@
+package Operators;
+
+public class BankAccount {
+    public static void main(String[] args) {
+        //Declares Variables
+        String accountHolderName = "Java";
+        double accountBalance = 5000.0;
+        double depositAmount = 1000.0;
+        double withdrawalAmount = 2500.0;
+        int numberOfTransactions = 0;
+        int minThreshold = 100;
+        //boolean isActive = true;
+        //Update the balance after deposit and withdrawal.
+        //Increase number of transactions with each operation.
+        //Deposit
+        accountBalance = accountBalance + depositAmount;
+        numberOfTransactions++;
+        //Withdrawl
+        accountBalance = accountBalance - withdrawalAmount;
+        numberOfTransactions++;
+
+        //Check if balance is below minimum threshold.
+        boolean belowMinimum = accountBalance > minThreshold;
+
+        //Verify if deposit is valid and account is active.
+        boolean accountIsActive = (depositAmount > 0) || (withdrawalAmount > 0);
+        boolean isValid = (depositAmount > 0) && accountIsActive;
+
+        //Print
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Final Balance: " + accountBalance);
+        System.out.println("Number of Transactions: " + numberOfTransactions);
+        System.out.println("Verify account is active:" + accountIsActive);
+        System.out.println("Verify if deposit is valid and account is active:" + isValid);
+    }
+}
---
+++ Operators/CalculateMonthlyEMI.java
@@ -0,0 +1,38 @@
+package Operators;
+
+public class CalculateMonthlyEMI {
+    public static void main(String[] args) {
+        String car = "Mahindra Scorpio";
+        float rating = 4.7f;
+        int reviews = 1;
+        int win = 1000;
+        String description = "The Scorpio is one of Mahindra's oldest nameplates in India.";
+
+
+        System.out.println("Car Name: " + car);
+        System.out.println("Rating: " + rating);
+        System.out.println(reviews + "K Reviews");
+        System.out.println("Rate & Win " + win);
+        System.out.println(description);
+
+        double onRoadPrice = 2190887;
+        double downPayment = 380000;
+        double loanAmount = onRoadPrice - downPayment;
+        double bankInterestRate = 9.5;
+        int loanPeriodYears = 4;
+        int loanMonths = loanPeriodYears * 12;
+
+        double monthlyRate = bankInterestRate / (12 * 100);  // 0.00791667
+
+        // EMI
+        double emi = (loanAmount * monthlyRate * Math.pow(1 + monthlyRate, loanMonths)) / (Math.pow(1 + monthlyRate, loanMonths) - 1);
+
+        System.out.println("Loan Amount: " + loanAmount);
+        System.out.println("Loan Period: " + loanPeriodYears);
+        System.out.println("Monthly EMI: " + emi);
+        System.out.println("Total Amount: " + onRoadPrice);
+
+
+
+    }
+}
